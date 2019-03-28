0b011005645d952237ab944d401308e8ecaabfca
luohuidong <luohuidong01@126.com>
2019-03-28 16:56:47 +0800

    添加 mysql 与 redis 配置

---
+++ config/redis.js
@@ -0,0 +1,16 @@
+const env = process.env.NODE_ENV
+
+let REDIS_CONF
+
+if (env === 'dev') {
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+}
+
+if (env === 'production') {
+  // do some thing...
+}
+
+module.exports = REDIS_CONF
